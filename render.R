56849fb01f5ecb4a2a809ffa615b15890cab2c6a
Bruno Grande <bgrande@sfu.ca>
2019-10-25 08:52:06 -0700

    Update lesson

---
+++ render.R
@@ -0,0 +1,20 @@
+output_dir <- "docs/"
+
+unlink(output_dir, recursive = TRUE)
+
+dir.create(output_dir)
+
+rmarkdown::render(input = "lesson.Rmd",
+                  output_format = "revealjs::revealjs_presentation",
+                  output_dir = output_dir,
+                  output_file = "slides.html")
+
+rmarkdown::render(input = "lesson.Rmd",
+                  output_format = "html_document",
+                  output_dir = output_dir,
+                  output_file = "index.html")
+
+rmarkdown::render(input = "setup.Rmd",
+                  output_format = "html_document",
+                  output_dir = output_dir,
+                  output_file = "setup.html")
